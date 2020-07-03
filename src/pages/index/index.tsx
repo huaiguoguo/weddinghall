cde9f976196883fa0cb873819b0d17fcb432edfc
guo <crazywork@foxmail.com>
2020-07-03 11:33:27 +0800

    prettierrc

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,21 +3,20 @@ import { View, Text } from '@tarojs/components'
 import './index.scss'
 
 export default class Index extends Component {
+  componentWillMount() {}
 
-  componentWillMount () { }
+  componentDidMount() {}
 
-  componentDidMount () { }
+  componentWillUnmount() {}
 
-  componentWillUnmount () { }
+  componentDidShow() {}
 
-  componentDidShow () { }
+  componentDidHide() {}
 
-  componentDidHide () { }
-
-  render () {
+  render() {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <Text>这是 hello world</Text>
       </View>
     )
   }
